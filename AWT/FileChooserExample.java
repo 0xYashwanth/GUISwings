9c2c9acebbbb8fcc4c3b41064cce2cffa784cfc0
Yashwanth Chandolu <0xyashwanth@gmail.com>
2023-07-17 03:42:58 +0530

    .

--- AWT/FileChooserExample.java
+++ AWT/FileChooserExample.java
@@ -22,33 +22,34 @@ public class FileChooserExample extends JFrame implements ActionListener {
         add(ta);
     }
 
-public void actionPerformed (ActionEvent e) {
-if(e.getSource() ==open){
-JFileChooser fc=new JFileChooser();
-int i=fc.showOpenDialog(this);
-if(i==JFileChooser.APPROVE_OPTION){
-File f=fc.getSelectedFile();
-String filepath=f.getPath();
-try{
-BufferedReader br=new BufferedReader(new FileReader(filepath));
-String s1="",s2="";
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == open) {
+            JFileChooser fc = new JFileChooser();
+            int i = fc.showOpenDialog(this);
+            if (i == JFileChooser.APPROVE_OPTION) {
+                File f = fc.getSelectedFile();
+                String filepath = f.getPath();
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(filepath));
+                    String s1 = "", s2 = "";
 
+                    while ((s1 = br.readLine()) != null) {
+                        s2 += s1 + "\n";
+                    }
+                    ta.setText(s2);
+                    br.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
-while((s1=br.readLine())!=null){
-    s2+=s1+"\n";
-}
-    ta.setText(s2);
-    br.close();
-}
-catch (Exception ex){ex.printStackTrace();}
-}
-}
-}
     public static void main(String[] args) {
-    FileChooserExample om=new FileChooserExample();
-    om.setSize(500,500);
-    om.setLayout(null);
-    om.setVisible(true);
-    om.setDefaultCloseOperation (EXIT_ON_CLOSE);
+        FileChooserExample om = new FileChooserExample();
+        om.setSize(500, 500);
+        om.setLayout(null);
+        om.setVisible(true);
+        om.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
