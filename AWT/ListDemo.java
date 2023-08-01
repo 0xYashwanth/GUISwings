cc58cafdd3406adaddbd5af428e20675ec51515d
Yashwanth Chandolu <0xyashwanth@gmail.com>
2023-08-01 22:25:49 +0530

    listdemo

---
+++ AWT/ListDemo.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+/* <applet code="ListDemo" width=300 height=180> </applet> */
+public class ListDemo extends Applet implements ActionListener 
+{	List os, browser;
+	String msg = "";
+	public void init() 
+	{	os = new List(4, false);
+		browser = new List(4, false);
+		// add items to os list
+		os.add("Windows XP");
+		os.add("Windows Vista");
+		os.add("Solaris");
+		os.add("Mac OS");
+		// add items to browser list
+		browser.add("Internet Explorer");
+		browser.add("Firefox");
+		browser.add("Opera");
+		//browser.select(1);
+// add lists to window
+		add(os);		
+		add(browser);
+		// register to receive action events
+		os.addActionListener(this);		
+		browser.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent ae) 
+	{	repaint();
+	}
+	public void paint(Graphics g) 
+	{	
+        //int idx[];
+		msg = "Current OS: ";
+		//idx = os.getSelectedIndexes();
+		//for(int i=0; i<idx.length; i++)
+		msg += os.getSelectedItem();
+		g.drawString(msg, 6, 120);
+		msg = "Current Browser: ";
+		msg += browser.getSelectedItem();
+		g.drawString(msg, 0, 140);
+	}
+}
